e6717b1ed4391fda3d207fda7568b427a63d495a
Aaron Marriner <amarriner@amarriner.com>
2014-01-23 07:05:21 -0700

    Fixed missing comma in player table SQL

--- sql/spelunky_players.sql
+++ sql/spelunky_players.sql
@@ -2,7 +2,7 @@ CREATE TABLE spelunky_players (
    players_id       BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    players_name     VARCHAR(30) NOT NULL,
    players_hashtag  VARCHAR(20) NOT NULL,
-   players_twitter  VARCHAR(30) NOT NULL
+   players_twitter  VARCHAR(30) NOT NULL,
    players_twitch   VARCHAR(100) NOT NULL
 );
    
